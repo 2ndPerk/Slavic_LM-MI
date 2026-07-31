c2c367dcc882a7417db6f53875bfcdc700d7ad5b
2ndPerk <twl@dal.ca>
2026-07-31 12:39:45 -0300

    post defense commit

--- SMI_Methods.py
+++ SMI_Methods.py
@@ -7,6 +7,21 @@ from scipy.optimize import linear_sum_assignment
 from torch.nn import CrossEntropyLoss
 import numpy as np
 
+from os import chdir
+from pathlib import Path
+
+
+def make_paths_relative_to_root():
+    """Always use the same, absolute (relative to root) paths
+
+    which makes moving the notebooks around easier.
+    """
+    top_level = Path(__file__).parent
+
+    chdir(top_level)
+
+
+make_paths_relative_to_root()
 
 #Data
 def prep_data(source, words):
